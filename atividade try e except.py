df014755619a24a8f4664f8828f1f72bf70c88e3
BrunCaiDev <amebamaior@gmail.com>
2025-06-05 15:33:22 -0300

    Update atividade try e except.py

--- atividade try e except.py
+++ atividade try e except.py
@@ -1,28 +1,28 @@
-Idade = 19
+Idade = 1
 try:
-    while Idade != 0:
+    while Idade <= 1:
         nome = str(input("Qual o seu nome?"))
         email = str(input("Digite o seu E-Mail:"))
         Idade = int(input("Digite a sua Idade:"))
     if Idade >= 18:
         print("Situação: Maior de Idade")
+     
+    elif Idade <= 0:
+     print("Por favor, insira uma idade valida.")
+
     else:
         print("Situação: Menor de Idade")
 
 except:
     print("Erro de caractere inválido!")
-
-    if Idade != 0:
-     print("Por favor, insira uma idade valida.")
     
-
-
 with open("Cadastros.txt" , "a") as cadastros:
     cadastros.write(f"=== Dados Cadastrados === \n Nome: {nome} \n E-mail: {email} \n Idade: {Idade} \n")
     if Idade <= 17:
         cadastros.write("Situacao: Menor de Idade")
-    elif Idade >= 18:
+    else: 
         cadastros.write("Situacao: Maior de Idade")
+       
 cadastros.close()
  
 
